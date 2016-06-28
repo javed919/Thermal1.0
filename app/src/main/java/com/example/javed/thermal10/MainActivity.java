9fdd7440890503d068229167f844b2a051c64ad0
Javed919 <muhammadjaved_919@hotmail.com>
2016-06-28 17:33:53 +0800

    TCP Client Added

--- app/src/main/java/com/example/javed/thermal10/MainActivity.java
+++ app/src/main/java/com/example/javed/thermal10/MainActivity.java
@@ -6,35 +6,71 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
     private static SeekBar seek_bar;
     private static TextView text_view;
     private static TextView user_id_disp;
 
+    Button buttonSubmit;
+    TextView response;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         user_id_disp = (TextView) findViewById(R.id.textView_UserID) ;
+        buttonSubmit =  (Button) findViewById(R.id.buttonSubmit);
+        response = (TextView) findViewById(R.id.responseTextView);
 
         user_id_disp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Pop.class));
+              //  startActivity(new Intent(MainActivity.this, Pop.class));
+                //getting value back
+                int requestCode=1;
+                Intent i = new Intent(MainActivity.this, Pop.class);
+                startActivityForResult(i,requestCode);
+                //------------------------------
+            }
+
+        });
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Client myClient = new Client("192.168.1.120",8000, response);
+                myClient.execute();
             }
         });
 
-        //blind listener
-      //  user_id_disp.setOnTouchListener(this);
-
         seekbbarr();
     }
+
+//    Public void onActivityReslut(requestCode,resultCode , i);
+//    {
+//        user_id_disp = (TextView) findViewById(R.id.textView_UserID) ;
+//        //checking with request we are responding to
+//        // if (requestCode == 1) {
+//        if (resultCode == RESULT_OK) {
+//            String userid = i.getStringExtra("edittextvalue");
+//            user_id_disp.setText(userid);
+//        }
+//        // }
+//    }
     public void seekbbarr(){
         seek_bar = (SeekBar)findViewById(R.id.seekBar);
         text_view = (TextView)findViewById(R.id.textView);
@@ -64,12 +100,35 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
     }
-    public void onSubmit(View v){
+
+
+
+    public void onSubmit(View v) throws IOException {
+        //client program for sending data
+        //Socket socket = new Socket("192.168.1.111", 8000);
+        //DataOutputStream DOS = new DataOutputStream(socket.getOutputStream());
+        //DOS.writeUTF("Data from Android");
+      //  socket.close();
+        // data sent
+
+        //client program for sending data
+        /**
+        Socket socket = new Socket("192.168.1.111", 55000);
+        OutputStream out = socket.getOutputStream();
+        PrintWriter output = new PrintWriter(out);
+        output.println("10");
+        output.flush();
+        output.close();
+        socket.close();
+        // data sent
+        **/
+
         Context context = MainActivity.this;
         CharSequence message = "Submited Successfully";
         int duration = Toast.LENGTH_SHORT;
         Toast.makeText(context , message ,duration).show();
     }
+
     public void onReset(View v){
         seek_bar.setProgress(50);
         seek_bar.refreshDrawableState();
--- app/src/main/java/com/example/javed/thermal10/Pop.java
+++ app/src/main/java/com/example/javed/thermal10/Pop.java
@@ -1,15 +1,23 @@
 package com.example.javed.thermal10;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Javed on 6/14/2016.
  */
 public class Pop extends Activity{
 
+    private static Button btn_cancel;
+    private static Button btn_ok;
+    private static EditText edit_text_user_ID;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -22,7 +30,36 @@ public class Pop extends Activity{
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout(width*6/10, height*6/10);
+        getWindow().setLayout(width*10/10, height*10/10);
+
+        //capturing user ID
+        edit_text_user_ID = (EditText) findViewById(R.id.editText_UserID);
+        //--------------------------------------------
+
+        //adding button  functions
+        btn_cancel = (Button) findViewById(R.id.btn_cancel_popup);
+        btn_ok = (Button) findViewById(R.id.btn_ok_popup);
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = edit_text_user_ID.getText().toString();
+                Intent intent = new Intent();
+                intent.putExtra("edittextvalue", data);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+        //------------------------------------------------------------
 
     }
+
 }
